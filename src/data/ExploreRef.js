a938990db4f4148d10bc72ed74d069b817608735
Raph <rafaelsprata@outlook.com>
2024-12-23 15:06:17 -0300

    adição da database e melhorias no icon do link para wpp

---
+++ src/data/ExploreRef.js
@@ -0,0 +1,95 @@
+
+const ExploreRef = {
+    Ref0: {
+        title: "Área de Estudo 0",
+        links: [
+          { to: 'img1', name: 'cerebro1' },
+          { to: 'img2', name: 'cerebro2' },
+          { to: 'img3', name: 'cerebro3' },
+          { to: 'img4', name: 'cerebro4' }
+        ]
+    },
+    Ref1: {
+        title: "Área de Estudo 1",
+        links: [
+          { to: 'img1', name: 'cerebro1' },
+          { to: 'img2', name: 'cerebro2' },
+          { to: 'img3', name: 'cerebro3' },
+          { to: 'img4', name: 'cerebro4' }
+        ]
+    },
+    Ref2: {
+        title: "Área de Estudo 0",
+        links: [
+          { to: 'img1', name: 'cerebro1' },
+          { to: 'img2', name: 'cerebro2' },
+          { to: 'img3', name: 'cerebro3' },
+          { to: 'img4', name: 'cerebro4' }
+        ]
+    },
+    Ref3: {
+        title: "Área de Estudo 0",
+        links: [
+          { to: 'img1', name: 'cerebro1' },
+          { to: 'img2', name: 'cerebro2' },
+          { to: 'img3', name: 'cerebro3' },
+          { to: 'img4', name: 'cerebro4' }
+        ]
+    },
+    Ref4: {
+        title: "Área de Estudo 0",
+        links: [
+          { to: 'img1', name: 'cerebro1' },
+          { to: 'img2', name: 'cerebro2' },
+          { to: 'img3', name: 'cerebro3' },
+          { to: 'img4', name: 'cerebro4' }
+        ]
+    },
+    Ref5: {
+        title: "Área de Estudo 0",
+        links: [
+          { to: 'img1', name: 'cerebro1' },
+          { to: 'img2', name: 'cerebro2' },
+          { to: 'img3', name: 'cerebro3' },
+          { to: 'img4', name: 'cerebro4' }
+        ]
+    },
+    Ref6: {
+        title: "Área de Estudo 0",
+        links: [
+          { to: 'img1', name: 'cerebro1' },
+          { to: 'img2', name: 'cerebro2' },
+          { to: 'img3', name: 'cerebro3' },
+          { to: 'img4', name: 'cerebro4' }
+        ]
+    },
+    Ref7: {
+        title: "Área de Estudo 0",
+        links: [
+          { to: 'img1', name: 'cerebro1' },
+          { to: 'img2', name: 'cerebro2' },
+          { to: 'img3', name: 'cerebro3' },
+          { to: 'img4', name: 'cerebro4' }
+        ]
+    },
+    Ref8: {
+        title: "Área de Estudo 0",
+        links: [
+          { to: 'img1', name: 'cerebro1' },
+          { to: 'img2', name: 'cerebro2' },
+          { to: 'img3', name: 'cerebro3' },
+          { to: 'img4', name: 'cerebro4' }
+        ]
+    },
+    Ref9: {
+        title: "Área de Estudo 0",
+        links: [
+          { to: 'img1', name: 'cerebro1' },
+          { to: 'img2', name: 'cerebro2' },
+          { to: 'img3', name: 'cerebro3' },
+          { to: 'img4', name: 'cerebro4' }
+        ]
+    },
+  };
+  
+  export default ExploreRef;
--- src/pages/Explore/Explore.js
+++ src/pages/Explore/Explore.js
@@ -1,10 +1,14 @@
 // React 
 import { useState } from 'react';
+
 // Style
 import './Explore.css';
 
 // Components
 import ExploreOption from '../../components/ExploreOption/ExploreOption';
+
+// Data
+import ExploreRef from '../../data/ExploreRef';
 
 // Framer Motion
 import { motion } from 'framer-motion';
@@ -29,165 +33,19 @@ const Explore = () => {
           <img src='/images/icon-button.png'  alt="icon" className='icon-explore'/>
         </motion.div>
         <div className="container-grid">
-            <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-1' 
-            navId='nav-1' 
-            links={[
-              {to: 'img1', name:'cerebro1'},
-              {to: 'img2', name:'cerebro2'},
-              {to: 'img3', name:'cerebro3'},
-              {to: 'img4', name:'cerebro4'},
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}
-            />
-            
-            <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-2' 
-            navId='nav-2' 
-            links={[
-              {to: 'img1', name:'cerebro3'},
-              {to: 'img2', name:'cerebro4'},
-              {to: 'img3', name:'cerebro4'},
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}
-            />
-            <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-3' 
-            navId='nav-3' 
-            links={[
-              {to: 'img1', name:'cerebro3'},
-              {to: 'img5', name:'cerebro5'},
-              {to: 'img5', name:'cerebro5'},
-              {to: 'img5', name:'cerebro5'},
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}           
-            />
-            <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-4' 
-            navId='nav-4' 
-            links={[
-              {to: 'img1', name:'cerebro3'},
-              {to: 'img1', name:'cerebro3'},
-
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}            
-            />
-            <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-5' 
-            navId='nav-5' 
-            links={[
-              {to: 'img1', name:'cerebro3'},
-              {to: 'img1', name:'cerebro3'},
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}            
-            />
-            <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-6' 
-            navId='nav-6' 
-            links={[
-              {to: 'img1', name:'cerebro3'},
-              {to: 'img1', name:'cerebro3'},
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}           
-            />
-            <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-7' 
-            navId='nav-7' 
-            links={[
-              {to: 'img1', name:'cerebro3'},
-              {to: 'img2', name:'cerebro4'},
-              {to: 'img3', name:'cerebro4'},
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}           
-            />
-            <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-8' 
-            navId='nav-8' 
-            links={[
-              {to: 'img1', name:'cerebro3'},
-              {to: 'img2', name:'cerebro4'},
-              {to: 'img3', name:'cerebro4'},
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}            
-            />
-            <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-9' 
-            navId='nav-9' 
-            links={[
-              {to: 'img1', name:'cerebro3'},
-              {to: 'img2', name:'cerebro4'},
-              {to: 'img3', name:'cerebro4'},
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}           
-            />
-            <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-10' 
-            navId='nav-10' 
-            links={[
-              {to: 'img1', name:'cerebro3'},
-              {to: 'img2', name:'cerebro4'},
-              {to: 'img3', name:'cerebro4'},
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}            
-            />
-            <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-11' 
-            navId='nav-11' 
-            links={[
-              {to: 'img1', name:'cerebro3'},
-              {to: 'img2', name:'cerebro4'},
-              {to: 'img3', name:'cerebro4'},
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}           
-            />
-                        <ExploreOption 
-            imgSrc='/images/2.jpg' 
-            imgId='img-12' 
-            navId='nav-12' 
-            links={[
-              {to: 'img1', name:'cerebro3'},
-              {to: 'img2', name:'cerebro4'},
-              {to: 'img3', name:'cerebro4'},
-            ]}
-            title='Imagem 1'
-            expandedMenu={expandedMenu}
-            setExpandedMenu={setExpandedMenu}           
-            />
-            
-          
+          {Object.values(ExploreRef).map(({ title, links }, index) => {
+              return (
+                <ExploreOption 
+                imgSrc={'/images/pagExplore/' + index + '.jpg'} 
+                imgId={'img-' + index} 
+                navId={'nav-1' + index} 
+                links={links}
+                title={title} 
+                expandedMenu={expandedMenu}
+                setExpandedMenu={setExpandedMenu}
+                />
+              );
+          })}       
         </div>
     </div>
   )
