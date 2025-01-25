57c038403933a54ec21fc10506d721396788e933
Rafael Souza <rafaelsprata@outlook.com>
2025-01-25 18:21:47 -0300

    finalização de cortes coronais - legendas

--- src/data/Anatomy/CC.js
+++ src/data/Anatomy/CC.js
@@ -2006,20 +2006,20 @@ const CC = [
             },
         ],
     },
-    {
-        name: "teste",
-        areas: [
-            {
-                shape: "rect", 
-                coords: [406,186,696,121], 
-                title: "", 
-                fillColor: "rgba(0, 0, 0, 0)",
-                strokeColor: "rgba(0, 0, 0, 0)",
-                description: "  ",
-                reference: ""
-            }, 
-        ],
-    },
+    // {
+    //     name: "teste",
+    //     areas: [
+    //         {
+    //             shape: "rect", 
+    //             coords: [406,186,696,121], 
+    //             title: "", 
+    //             fillColor: "rgba(0, 0, 0, 0)",
+    //             strokeColor: "rgba(0, 0, 0, 0)",
+    //             description: "  ",
+    //             reference: ""
+    //         }, 
+    //     ],
+    // },
 ];
 
   export default CC
